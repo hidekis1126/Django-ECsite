db035fd5540a749cf218d821992b274365482b52
hidekis1126 <consulart.hmk@gmail.com>
2022-02-18 07:03:13 +0900

    ECサイト構築

---
+++ app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from app import views
+
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('product/<slug>', views.ItemDetailView.as_view(), name='product'),
+]
---
+++ app/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.views.generic import View
+from .models import Item
+
+
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        item_data = Item.objects.all()
+        return render(request, 'app/index.html', {
+            'item_data': item_data
+        })
+
+class ItemDetailView(View):
+    def get(self, request, *args, **kwargs):
+        item_data = Item.objects.get(slug=self.kwargs['slug'])
+        return render(request, 'app/product.html', {
+            'item_data': item_data
+        })
